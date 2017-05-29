95eaa2d62101ab78a525b50ac4f3fe0c0a79658d
Mikkel Sjølin <m.sjolin@gmail.com>
2017-05-29 22:56:16 +0200

    WIP

--- src/android/CordovaDigits.java
+++ src/android/CordovaDigits.java
@@ -39,7 +39,7 @@ public class CordovaDigits extends CordovaPlugin {
     super.initialize(cordova, webView);
 
     TwitterAuthConfig authConfig = getTwitterConfig();
-    Fabric.with(cordova.getActivity().getApplicationContext(), new Crashlytics(), new TwitterCore(authConfig), new Digits());
+    Fabric.with(new Fabric.Builder(this).debuggable(true).kits(new Crashlytics(), new TwitterCore(authConfig), new Digits.Builder().build()).build()));
   }
 
   @Override
